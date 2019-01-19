31cd9f67633d8e3c04bebdd1f74e44d9636c2bd6
Sergey Onufrienko <sergey@onufrienko.com>
2019-01-19 23:49:58 +0200

    Use port

--- load-test.sh
+++ load-test.sh
@@ -2,9 +2,9 @@
 
 IP=$1
 
-URL1="http://$IP/empty"
-URL2="http://$IP/books/1"
-URL3="http://$IP/books"
+URL1="http://$IP:3000/empty"
+URL2="http://$IP:3000/books/1"
+URL3="http://$IP:3000/books"
 
 sleep 5
 wrk -d5m -c500 -t2 $URL1
